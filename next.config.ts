b4deb80de2b4aefd6d6c66fd9971767820b31cc2
Oleg Malyavin <oliko.brain@gmail.com>
2025-06-10 22:03:32 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import {PHASE_DEVELOPMENT_SERVER} from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = async (phase: string): Promise<NextConfig> => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  return {
+    output: 'export',
+    images: {unoptimized: true},
+    assetPrefix: isDev ? undefined : process.env.NEXT_PUBLIC_DOMAIN,
+    basePath: isDev ? undefined : process.env.NEXT_PUBLIC_BASE_PATH,
+  }
+}
 
 export default nextConfig;
